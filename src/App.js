ac6fa0ccdf52b6cfc2ea1ef59be66aa4d0dc35a2
zentoc <christian.bering.jensen@gmail.com>
2020-06-26 11:01:50 +0200

    First change

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Fodims sød fodom lige så sød.
           </p>
           <a
             className="App-link"
